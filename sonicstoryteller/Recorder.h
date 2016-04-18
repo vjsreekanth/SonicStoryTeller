7c8b365925a1f0793c297c4d0e2d25d3b4d548db
vjsreekanth <vjsreekanth@gmail.com>
2016-04-18 16:36:47 -0700

    Created Recorder Class

---
+++ sonicstoryteller/Recorder.h
@@ -0,0 +1,13 @@
+//
+//  Recorder.h
+//  sonicstoryteller
+//
+//  Created by SREEKANTH JAGADEESAN VAZHAPPULLY on 2016-04-18.
+//  Copyright © 2016 SREEKANTH JAGADEESAN VAZHAPPULLY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Recorder : NSObject
+
+@end
---
+++ sonicstoryteller/Recorder.m
@@ -0,0 +1,13 @@
+//
+//  Recorder.m
+//  sonicstoryteller
+//
+//  Created by SREEKANTH JAGADEESAN VAZHAPPULLY on 2016-04-18.
+//  Copyright © 2016 SREEKANTH JAGADEESAN VAZHAPPULLY. All rights reserved.
+//
+
+#import "Recorder.h"
+
+@implementation Recorder
+
+@end
